5c5525c51497e861a1e8fb0d937ca85107364fa8
tavolafourcade <tavolafourcade@gmail.com>
2023-05-29 14:17:00 -0500

    [ ADD ] Fetching and Rendering Posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import PostCreate from './PostCreate'
+import PostList from './PostList'
 
 function App() {
 
@@ -7,6 +8,10 @@ function App() {
     <div className='container'>
       <h1>Create Post</h1>
       <PostCreate/>
+      <hr/>
+      <h1>Posts</h1>
+
+      <PostList/>
     </div>
   )
 }
