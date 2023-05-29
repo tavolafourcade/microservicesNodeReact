bdfee2c7a45af8d7ed788e6691a781d646a9fd50
tavolafourcade <tavolafourcade@gmail.com>
2023-05-29 12:25:25 -0500

    [ ADD ] Client React project [ ADD ] PostCreate component

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PostCreate from './PostCreate'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate/>
+    </div>
+  )
+}
+
+export default App
