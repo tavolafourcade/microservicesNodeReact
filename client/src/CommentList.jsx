152d4d99ba80841688fd6c6227de99ed37359530
tavolafourcade <tavolafourcade@gmail.com>
2023-05-31 18:20:16 -0500

    [ UPDATE ] Using the Query Service to fetch the Posts with their Comments

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,19 +1,16 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+/* eslint-disable react/prop-types */
+const CommentList = ({comments}) => {
+  // const [comments, setComments] = useState([])
 
-// eslint-disable-next-line react/prop-types
-const CommentList = ({postId}) => {
-  const [comments, setComments] = useState([])
+  // const fetchData = async () => {
+  //   const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
 
-  const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+  //   setComments(res.data)
+  // }
 
-    setComments(res.data)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   fetchData()
+  // }, [])
   
   const renderedComments = comments.map(comment => {
     return (
