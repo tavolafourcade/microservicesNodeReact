1c86fed4c6b654f2a3cb174b65c991d0b09eb699
tavolafourcade <tavolafourcade@gmail.com>
2023-05-31 00:34:28 -0500

    [ UPDATE ] Query Service

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,6 +14,8 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
+
+  // Query Service
   axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
   });
