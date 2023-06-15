163f244b9f95f1ac6875228b6942d948bafa856f
tavolafourcade <tavolafourcade@gmail.com>
2023-06-15 10:28:37 -0500

    [ FIX ] Problem with Postman not receiving response to POST request

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,39 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-const events = []
+const events = [];
 
-app.post('/events', (req, res) => {
-  const event = req.body
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-  events.push(event)
+  events.push(event);
 
-  axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://comments-srv:4001/events', event).catch((err) => {
-    console.log(err.message);
-  });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  res.send({ status: "OK" });
+});
 
-  // Query Service
-  axios.post('http://query-srv:4002/events', event).catch((err) => {
-    console.log(err.message);
-  });
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
-  // Moderation Service
-  axios.post('http://moderation-srv:4003/events', event).catch((err) => {
-    console.log(err.message);
-  });
-
-  res.send({ status: 'OK' })
-})
-
-app.get('/events', (req, res) => {
-  res.send(events)
-})
-
-app.listen(4005, () => console.log('Listening on 4005'))
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
--- posts/index.js
+++ posts/index.js
@@ -1,43 +1,46 @@
-const express = require('express')
-const { randomBytes } = require('crypto')
-const bodyParser = require('body-parser')
-const axios = require('axios')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {}
+const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post('/posts', async (req, res) => {
-  const id = randomBytes(4).toString('hex')
-  const { title } = req.body
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
   posts[id] = {
-    id, title
-  }
+    id,
+    title,
+  };
 
-  await axios.post('http://event-bus-srv:4005/events', {
-    type: 'PostCreated',
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "PostCreated",
     data: {
-      id, title
-    }
-})
+      id,
+      title,
+    },
+  });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type)
+  res.status(201).send(posts[id]);
+});
 
-  res.send({})
-})
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
 
-  res.status(201).send(posts[id])
-})
+  res.send({});
+});
 
 app.listen(4000, () => {
-  console.log('Listening on 4000')
-})
+  console.log('v55')
+  console.log("Listening on 4000");
+});
